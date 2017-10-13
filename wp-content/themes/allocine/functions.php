398325696dcb7504f6086b7fb66e783d187e53b1
gregoirepuget <gregoire@36pixels.fr>
2017-10-13 12:14:37 +0200

    ajout des styles et scripts

---
+++ wp-content/themes/allocine/functions.php
@@ -0,0 +1,28 @@
+<?php
+function ajout_scripts() {
+
+// enregistrement d'un nouveau script
+wp_register_script('bootstrap_script', get_template_directory_uri() . '/assets/scripts/bootstrap.min.js', array('jquery'),'1.1', true);
+
+// appel du script dans la page
+wp_enqueue_script('bootstrap_script');
+
+wp_register_script('main_script', get_template_directory_uri() . '/assets/scripts/main.js', array('jquery'),'1.1', true);
+wp_enqueue_script('main_script');
+
+
+wp_register_style( 'googlefont_style', 'https://fonts.googleapis.com/css?family=Arapey|Hammersmith+One' );
+wp_enqueue_style( 'googlefont_style' );
+
+// enregistrement d'un nouveau style
+wp_register_style( 'bootstrap_style', get_template_directory_uri() . '/assets/styles/bootstrap.min.css' );
+
+// appel du style dans la page
+wp_enqueue_style( 'bootstrap_style' );
+
+wp_register_style( 'main_style', get_template_directory_uri() . '/assets/styles/main.css' );
+wp_enqueue_style( 'main_style' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
